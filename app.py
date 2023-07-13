261da2761f97ff86f5242d33e1508604d3cb697c
rahul <rahulankola13@gmail.com>
2023-07-14 01:57:17 +0800

    Form submission, excel updation complete, working

--- app.py
+++ app.py
@@ -28,35 +28,27 @@ def predict():
 
 @app.route('/form1data', methods =['GET','POST'])
 def form1():
-
     date = request.form['date']
-    itemid = request.form['itemid']
-    quantiy = request.form['quantity']
-    desktop_path = os.path.expanduser('~/Desktop')
+    item_id = request.form['itemid']
+    quantity = request.form['quantity']
 
-    os.makedirs(desktop_path, exist_ok=True)
-    file_path = os.path.join(desktop_path, 'data.csv')
+    file_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'data.csv')
+    file_exists = os.path.isfile(file_path)
 
-    if not (os.path.exists(f'{desktop}/data.csv')):
-        
-        data = np.array([
-            ['id', 'date', 'store','item','sales'],
-            ['0', date, '1',itemid,quantiy]
-        ])
+    with open(file_path, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
 
-        with open(file_path, mode='w', newline='') as file:
-            writer = csv.writer(file)
-            writer.writerows(data)
-    else:
-        num_rows=0
-        with open(file_path, mode='r') as file:
-            reader = csv.reader(file)
-            next(reader)
-            num_rows = sum(1 for row in reader)
+        if not file_exists:
+            writer.writerow(['id', 'date', 'store', 'item', 'sales'])
 
-        with open(file_path, mode='a') as file:
-             writer = csv.writer(file)
-             writer.writerow([num_rows, date, 1, itemid, quantiy])
+        if file_exists:
+            with open(file_path, 'r') as csvfile:
+                last_id = sum(1 for _ in csvfile) - 1
+        else:
+            last_id = -1
+
+        new_id = last_id + 1
+        writer.writerow([new_id, date, 1, item_id, quantity])
 
     return render_template("sales.html")
 
@@ -64,25 +56,33 @@ def form1():
 
 @app.route('/form2data', methods=['POST'])
 def form2():
-    datafile = request.files['datafile']
-    df = pd.read_csv(datafile)
+    file_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'data.csv')
+    file_exists = os.path.isfile(file_path)
 
-    desktop_path = os.path.expanduser('~/Desktop')
-    os.makedirs(desktop_path, exist_ok=True)
-    file_path = os.path.join(desktop_path, 'data.csv')
+    with open(file_path, 'a', newline='') as csvfile:
+        writer = csv.writer(csvfile)
 
-    num_rows=0
-    with open(file_path, mode='r') as file:
-        reader = csv.reader(file)
-        next(reader)
-        num_rows = sum(1 for row in reader)
+        if not file_exists:
+            writer.writerow(['id', 'date', 'store', 'item', 'sales'])
 
-    for i in range(0, df.shape[0]):
-        with open(file_path, mode='a') as file:
-            print(df.shape)
-            print(num_rows)
-            writer = csv.writer(file)
-            writer.writerow([num_rows, df.iloc[i,0], 1, df.iloc[i,1], df.iloc[i,2]]) #id date store itemid sales
+        if file_exists:
+            with open(file_path, 'r') as csvfile:
+                last_id = sum(1 for _ in csvfile) - 1
+        else:
+            last_id = -1
+
+        datafile = request.files['datafile']
+        if datafile:
+            reader = csv.reader(datafile.read().decode('utf-8').splitlines())
+            next(reader)
+            for row in reader:
+                date = row[0]
+                item_id = row[1]
+                quantity = row[2]
+
+                new_id = last_id + 1
+                writer.writerow([new_id, date, 1, item_id, quantity])
+                last_id += 1
 
     return render_template("sales.html")
 
