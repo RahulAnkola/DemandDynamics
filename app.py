4fca363c769a7449ed5114653d21d824348b0d94
rahul <rahulankola13@gmail.com>
2023-07-14 01:43:43 +0800

    forms error

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, render_template
-import cv2
+import csv
 import os
-
-import openpyxl
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop\\')
@@ -21,34 +21,71 @@ def sales():
 def predict():
     if not (os.path.exists(f'{desktop}/data.csv')):
         return 'Data file does not exist. Please enter data first'
+    else:
+        print()
+        #run model get results
+
 
 @app.route('/form1data', methods =['GET','POST'])
 def form1():
+
     date = request.form['date']
     itemid = request.form['itemid']
     quantiy = request.form['quantity']
+    desktop_path = os.path.expanduser('~/Desktop')
+
+    os.makedirs(desktop_path, exist_ok=True)
+    file_path = os.path.join(desktop_path, 'data.csv')
 
     if not (os.path.exists(f'{desktop}/data.csv')):
-        workbook = openpyxl.Workbook()
-        sheet = workbook.active
-
-        sheet.cell(row=1, column=1, value="id")
-        sheet.cell(row=1, column=2, value="date")
-        sheet.cell(row=1, column=3, value="store")
-        sheet.cell(row=1, column=4, value="item")
-        sheet.cell(row=1, column=5, value="sales")
         
-        sheet.cell(row=2, column=1, value=0)
-        sheet.cell(row=2, column=2, value=date)
-        sheet.cell(row=2, column=3, value=1)
-        sheet.cell(row=2, column=4, value=itemid)
-        sheet.cell(row=2, column=5, value=quantiy)
-        
-        
+        data = np.array([
+            ['id', 'date', 'store','item','sales'],
+            ['0', date, '1',itemid,quantiy]
+        ])
+
+        with open(file_path, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerows(data)
+    else:
+        num_rows=0
+        with open(file_path, mode='r') as file:
+            reader = csv.reader(file)
+            next(reader)
+            num_rows = sum(1 for row in reader)
+
+        with open(file_path, mode='a') as file:
+             writer = csv.writer(file)
+             writer.writerow([num_rows, date, 1, itemid, quantiy])
+
+    return render_template("sales.html")
 
 
-    #return  str(type(date))+ '  ' + str(type(itemid)) + '  ' + str(type(quantiy))
-    
+
+@app.route('/form2data', methods=['POST'])
+def form2():
+    datafile = request.files['datafile']
+    df = pd.read_csv(datafile)
+
+    desktop_path = os.path.expanduser('~/Desktop')
+    os.makedirs(desktop_path, exist_ok=True)
+    file_path = os.path.join(desktop_path, 'data.csv')
+
+    num_rows=0
+    with open(file_path, mode='r') as file:
+        reader = csv.reader(file)
+        next(reader)
+        num_rows = sum(1 for row in reader)
+
+    for i in range(0, df.shape[0]):
+        with open(file_path, mode='a') as file:
+            print(df.shape)
+            print(num_rows)
+            writer = csv.writer(file)
+            writer.writerow([num_rows, df.iloc[i,0], 1, df.iloc[i,1], df.iloc[i,2]]) #id date store itemid sales
+
+    return render_template("sales.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
